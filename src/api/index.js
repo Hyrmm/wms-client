96d29507fe0fd439b44bd3846f6e390744a3bfe9
Hyrm <1358188945@qq.com>
2023-04-06 22:15:18 +0800

    环境配置调整

--- src/api/index.js
+++ src/api/index.js
@@ -15,10 +15,10 @@ import App from "@/main"
 //环境
 let baseURL
 
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV == "online") {
   baseURL = "http://43.138.122.88:5000/api"
 } else if (process.env.NODE_ENV == "production") {
-  baseURL = "http://101.33.232.145:5000/api"
+  baseURL = "http://43.138.122.88:5000/api"
 }
 else {
   baseURL = "http://127.0.0.1:5000/api"
